ef8d6353797ba28a4fbe722d65f1b5782fd09f8a
azkaen <azkaen1973@gmail.com>
2026-07-06 04:40:41 -0500

    Add: ssp total portfolio with 20 activities and 96 photos

---
+++ ssp total/js/script.js
@@ -0,0 +1,14 @@
+// Progress bar
+window.addEventListener('scroll', () => {
+    const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    document.getElementById('progressBar').style.width = ((winScroll / height) * 100) + '%';
+});
+
+// Smooth scroll
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
+    });
+});
